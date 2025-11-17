b69e6760be812a5bbadca00bd1535a68701bc02c
CarodDannit <imran.danial2006@gmail.com>
2025-11-17 15:23:56 +0800

    Refactor Car class to include super constructor and override describe

--- Car.java
+++ Car.java
@@ -6,15 +6,10 @@ public class Car extends Vehicle{
 
     }
 
-    public Car(String plateNum, String manufacturer, int noOfPassenger, int x1, int y1, int x2, int y2){
-        this.setPlateNum(plateNum);
-        this.setManufacturer(manufacturer);
+    public Car(String plateNum, String manufacturer, int noOfPassenger, double x1, double y1, double x2, double y2){
+        super(plateNum, manufacturer, x1, y1, x2, y2);
         this.noOfPassenger = noOfPassenger;
-        this.setX1(x1);
-        this.setY1(y1);
-        this.setX2(x2);
-        this.setY2(y2);
-
+        
     }
 
     public void setNoOfPassenger(int noOfPassenger){
@@ -25,13 +20,15 @@ public class Car extends Vehicle{
         return this.noOfPassenger;
     }
 
+    @Override
     public void describe(){
         System.out.println("plateNum: " + getPlateNum());
         System.out.println("manufacturer: " + getManufacturer());
         System.out.println("number of passenger: " + getNoOfPassenger());
         System.out.println("start coord: " + getX1() + ", " + getY1());
         System.out.println("last coord: " + getX2() + ", " + getY2());
+        System.out.println("fuel for 100km: " + getFuelFor100km());
         System.out.printf("distance: %.2f km\n", findDistance());
-        System.out.printf("required fuel: %.2f L\n\n", findRequiredFuel());
+        System.out.printf("required fuel: %.1f L\n\n", findRequiredFuel());
     }
 }

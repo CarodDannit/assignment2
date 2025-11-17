029a7de1da782adfea5ea602d92250430dcbcc85
CarodDannit <imran.danial2006@gmail.com>
2025-11-17 15:24:19 +0800

    Refactor Truck class to extend Vehicle and update methods

--- Truck.java
+++ Truck.java
@@ -6,15 +6,9 @@ public class Truck extends Vehicle{
 
     }
 
-    public Truck(String plateNum, String manufacturer, int loadCapacity, int x1, int y1, int x2, int y2){
-        this.setPlateNum(plateNum);
-        this.setManufacturer(manufacturer);
+    public Truck(String plateNum, String manufacturer, int loadCapacity, double x1, double y1, double x2, double y2){
+        super(plateNum, manufacturer, x1, y1, x2, y2);
         this.loadCapacity = loadCapacity;
-        this.setX1(x1);
-        this.setY1(y1);
-        this.setX2(x2);
-        this.setY2(y2);
-
     }
 
     public void setLoadCapacity(int loadCapacity){
@@ -25,12 +19,14 @@ public class Truck extends Vehicle{
         return this.loadCapacity;
     }
 
+    @Override
     public void describe(){
         System.out.println("plateNum: " + getPlateNum());
         System.out.println("manufacturer: " + getManufacturer());
         System.out.println("load capacity: " + getLoadCapacity());
         System.out.println("start coord: " + getX1() + ", " + getY1());
         System.out.println("last coord: " + getX2() + ", " + getY2());
+        System.out.println("fuel for 100km: " + getFuelFor100km());
         System.out.printf("distance: %.2f km\n", findDistance());
         System.out.printf("required fuel: %.2f L\n\n", findRequiredFuel());
     }

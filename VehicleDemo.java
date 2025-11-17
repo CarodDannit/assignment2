3432696dd19e6c3366fd2dcbbb2f4f567330c3e4
CarodDannit <imran.danial2006@gmail.com>
2025-11-17 15:25:33 +0800

    Modify VehicleDemo to enhance Car initialization
    Updated Car object initialization with new parameters and added output header.

--- VehicleDemo.java
+++ VehicleDemo.java
@@ -1,7 +1,10 @@
 public class VehicleDemo {
     public static void main(String[] args) {
-        Car Aruz = new Car("MMM 6055", "Perodua", 7, 50, 350, 89, 15);
+        // question A
+        Car Aruz = new Car("MMM 6055", "Perodua", 7, 50.1, 350.3, 89.3, 15.2);
 
+        // question B
+        System.out.println("===== Aruz =====");
         Aruz.setFuelFor100km(4.5);
         Aruz.describe();
     }

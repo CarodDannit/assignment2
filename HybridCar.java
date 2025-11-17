01918ca18011cef6004289f3f729b59fd28415e0
CarodDannit <imran.danial2006@gmail.com>
2025-11-17 15:24:48 +0800

    Refactor HybridCar constructor and describe method
    Updated constructor to call super and changed parameter types for coordinates.

--- HybridCar.java
+++ HybridCar.java
@@ -6,15 +6,10 @@ public class HybridCar extends Car{
 
     }
 
-    public HybridCar(String plateNum, String manufacturer, int noOfPassenger, int batteryCapacity, int x1, int y1, int x2, int y2){
-        this.setPlateNum(plateNum);
-        this.setManufacturer(manufacturer);
-        this.setNoOfPassenger(noOfPassenger);
+    public HybridCar(String plateNum, String manufacturer, int noOfPassenger, int batteryCapacity, double x1, double y1, double x2, double y2){
+        super(plateNum, manufacturer, noOfPassenger, x1, y1, x2, y2);
         this.batteryCapacity = batteryCapacity;
-        this.setX1(x1);
-        this.setY1(y1);
-        this.setX2(x2);
-        this.setY2(y2);
+        
     }
 
     public void setBatteryCapacity(int batteryCapacity){
@@ -25,6 +20,7 @@ public class HybridCar extends Car{
         return this.batteryCapacity;
     }
 
+    @Override
     public void describe(){
         System.out.println("plateNum: " + getPlateNum());
         System.out.println("manufacturer: " + getManufacturer());
@@ -32,7 +28,8 @@ public class HybridCar extends Car{
         System.out.println("battery capacity: " + getBatteryCapacity());
         System.out.println("start coord: " + getX1() + ", " + getY1());
         System.out.println("last coord: " + getX2() + ", " + getY2());
+        System.out.println("fuel for 100km: " + getFuelFor100km());
         System.out.printf("distance: %.2f km\n", findDistance());
-        System.out.printf("required fuel: %.2f L\n\n", findRequiredFuel());
+        System.out.printf("required fuel: %.1f L\n\n", findRequiredFuel());
     }
 }
